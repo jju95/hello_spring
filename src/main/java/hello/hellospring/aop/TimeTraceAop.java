c1c78507b53d72c324005f44107fb8c4419b3f47
Kim Hee ju <jju95@gmail.com>
2022-08-26 18:03:49 +0900

    aop

--- src/main/java/hello/hellospring/aop/TimeTraceAop.java
+++ src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -4,16 +4,20 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.jboss.logging.Logger;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
 public class TimeTraceAop {
 
-    @Around("execution(* hello.hellospring..*(..))")
+    @Around("execution(* hello.hellospring..*(..))") // targeting용
     public Object execute(ProceedingJoinPoint joinPoint) throws Throwable{
-        // joinPointㄱㅏ 멀까,,,,, 시간계산하는건 알겠눈데ㅠ슈
+        // method 실행시 항상 걸림
+
         long start = System.currentTimeMillis();
         System.out.println("start : "+joinPoint.toString());
         try{
+            // 조건문 생성후 예외처리 가능
             return joinPoint.proceed();
         }finally {
             long end = System.currentTimeMillis();
@@ -21,6 +25,5 @@ public class TimeTraceAop {
             System.out.println("end : "+joinPoint.toString()+" , ms : "+timeMs );
         }
     }
-
 
 }
